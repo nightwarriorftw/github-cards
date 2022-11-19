0beee6f3f1e2af397dd67915ed42eb5f335031b2
nightwarriorftw <amanv1999@gmail.com>
2022-11-19 17:18:01 +0530

    Make http-equiv camel case

--- components/CardApp.tsx
+++ components/CardApp.tsx
@@ -25,8 +25,8 @@ const CardApp: NextPage = () => {
         <meta name="keywords" content="" />
         <meta name="description" content="Github App" />
         <meta http-equiv="X-UA-Compatible" content="IE=7" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Github App</title>
       </Head>
